ea91b6572a01e1e1586517451a970f08d5df38b1
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-12-05 20:02:18 +0530

    new app and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { DepartmentComponent } from './department/department.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  // template: `<p>employee
+  //   works!</p>`,
+  styleUrls: ['./employee.component.scss']
+  // styles: []
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
